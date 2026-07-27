2884aa81641166b74b1c72d19889ce23b344c670
Victor <victorcriri313@gmail.com>
2026-07-27 16:13:03 -0300

    feat: começa tela 404

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import styles from "./not-found.module.css";
+
+export default function notFound() {
+    return (
+        <main className={styles.main}>
+            <p>404</p>
+        </main>
+    );
+}
