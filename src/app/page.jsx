5ffe0f593a5a33da56f9a1eaddc8eada9cc2bca7
Victor <victorcriri313@gmail.com>
2026-07-27 21:02:37 -0300

    feat: deixa codigo em uma unica section para ocupar tela inteira

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,27 +5,36 @@ import recursos from "../data/recursos.json";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <section className={styles.tituloContainer}>
-                <h1>Next.js</h1>
-                <div className={styles.separador} />
-                <p>pesquisa sobre o Next.js</p>
-            </section>
-
-            <section className={styles.introducaoContainer}>
-                <p>O Next.js é um framework web de código aberto baseado em React criado pela Vercel que adiciona estrutura, renderização avançada e facilidades de SEO a aplicações. Ele permite criar sites e sistemas completos (fullstack) de forma rápida e organizada.</p>
-            </section>
-
-            <section className={styles.recursosContainer}>
-                <div>
-                    <h2 className={styles.recursosTitulo}>Principais Recursos</h2>
+            <section className={styles.homeSection}>
+                <div className={styles.tituloContainer}>
+                    <h1>Next.js</h1>
+                    <div className={styles.separador}></div>
+                    <p>Pesquisa sobre o Next.js</p>
                 </div>
-                <div className={styles.cards}>
-                    {recursos.map(card => <Card key={card.id} titulo={card.titulo} imagem={card.imagem}  conteudo={card.conteudo} />)}
+                <div className={styles.introducaoContainer}>
+                    <p>
+                        O Next.js é um framework web de código aberto baseado em
+                        React criado pela Vercel que adiciona estrutura,
+                        renderização avançada e facilidades de SEO a aplicações.
+                        Ele permite criar sites e sistemas completos (fullstack)
+                        de forma rápida e organizada.
+                    </p>
                 </div>
-            </section>
-
-            <section>
-
+                <div className={styles.recursosContainer}>
+                    <h2 className={styles.recursosTitulo}>
+                        Principais Recursos
+                    </h2>
+                    <div className={styles.cards}>
+                        {recursos.map((card) => (
+                            <Card
+                                key={card.id}
+                                titulo={card.titulo}
+                                imagem={card.imagem}
+                                conteudo={card.conteudo}
+                            />
+                        ))}
+                    </div>
+                </div>
             </section>
 
         </main>
