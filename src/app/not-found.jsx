72cd77e7fd76ac0109b5cf31192a66600d237105
Victor <victorcriri313@gmail.com>
2026-08-03 08:29:12 -0300

    feat: finaliza tela 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,14 @@
 import styles from "./not-found.module.css";
+import Link from "next/link";
 
 export default function notFound() {
     return (
         <main className={styles.main}>
-            <p>404</p>
+            <div className={styles.textosContainer}>
+                <p>404</p>
+                <p>Página não encontrada</p>
+            </div>
+            <Link className={styles.botao} href="/">Voltar para Home</Link>
         </main>
     );
 }
